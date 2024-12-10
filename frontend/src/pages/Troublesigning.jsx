7cdbc35ef3bdaad01bfb0e6c8345f2562da4579e
Priyansh-max <priyanshagarwal3381@gmail.com>
2024-12-10 19:47:15 +0530

    cleaning

--- frontend/src/pages/Troublesigning.jsx
+++ frontend/src/pages/Troublesigning.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useState } from "react";
 import Header from "../components/Header";
